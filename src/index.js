516294c3384891b2ba362b64a14511d9d4035bc3
angryong91 <91angryong@gmail.com>
2024-05-20 17:43:31 +0900

    cleanup function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Cleanup from "./Cleanup";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <div>
+    <App />
+    <Cleanup />
+  </div>
+);
