0ba0138bd2cb702c14c3199f282fb446ac1c7fe6
angryong91 <91angryong@gmail.com>
2024-05-21 13:54:37 +0900

    to do list

---
+++ src/Todolist.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Todolist() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    // form 제출 금지, 즉 refresh되지 않고 다른 동작을 수행할 수 있다
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((current) => [toDo, ...current]);
+    setToDo("");
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      {/* form에 button 클릭시 자동으로 refresh됨 
+      button의 기본 타입은 submit이다 */}
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
+
+export default Todolist;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import Cleanup from "./Cleanup";
+// import App from "./App";
+// import Cleanup from "./Cleanup";
+import Todolist from "./Todolist";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <App />
-    <Cleanup />
+    {/* <App />
+    <Cleanup /> */}
+    <Todolist />
   </div>
 );
