bc4921478753316d5db37ec9d9d27c80e416aa2a
angryong91 <91angryong@gmail.com>
2024-05-21 14:20:58 +0900

    to do list second

--- src/Todolist.js
+++ src/Todolist.js
@@ -28,6 +28,12 @@ function Todolist() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
